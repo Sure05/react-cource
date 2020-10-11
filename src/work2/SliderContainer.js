bec2322bc1fc9f85f336db950c76ccdd07a01f38
sure05 <deurlex@gmail.com>
2020-10-11 20:37:44 +0300

    last update hw2

--- src/work2/SliderContainer.js
+++ src/work2/SliderContainer.js
@@ -1,6 +1,7 @@
 import React, {Fragment, Component} from 'react';
 import SplideComponent from "./SplideComponent";
 import {Checkbox, Input, Select} from 'semantic-ui-react'
+import './style.css'
 
 class SliderContainer extends Component {
 	state = {
@@ -19,12 +20,15 @@ class SliderContainer extends Component {
 			{value: 'loop', text: 'loop' },
 		]
 		return (
-			<Fragment>
+			<div className="container">
 				<Input value={this.state.perPage} type="number" placeholder='Search...' onChange={(e, {value}) => this.setState({perPage: value})} />
+				<br/><br/>
 				<Checkbox checked={this.state.autoplay} label={"Autoplay"} onClick={(e, {checked}) => this.setState({autoplay: checked})} />
+				<br/><br/>
 				<Select value={this.state.type} placeholder='Select your country' onChange={(e, {value}) => this.setState({type: value})} options={countryOptions} />
+				<br/><br/>
 				<SplideComponent options={this.state}/>
-			</Fragment>
+			</div>
 		);
 	}
 }
